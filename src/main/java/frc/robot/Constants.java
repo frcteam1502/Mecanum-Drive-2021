009aa50f731ba535b2be29e251c90a6b3b5eae22
Colby Pfeiffer <Colbyjpfeiffer@gmail.com>
2021-12-06 07:59:14 -0500

    first commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,30 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+    public static final class Motors {
+        public static final CANSparkMax DRIVE_FRONT_LEFT = new CANSparkMax(0, MotorType.kBrushless);
+        public static final CANSparkMax DRIVE_FRONT_RIGHT = new CANSparkMax(1, MotorType.kBrushless);
+        public static final CANSparkMax DRIVE_BACK_LEFT = new CANSparkMax(2, MotorType.kBrushless);
+        public static final CANSparkMax DRIVE_BACK_RIGHT = new CANSparkMax(3, MotorType.kBrushless);
+    }
+    public static final class Joysticks {
+        public static final Joystick RIGHT_JOYSTICK = new Joystick(0);
+    }
+}
